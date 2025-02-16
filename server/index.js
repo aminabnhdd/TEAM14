e224b0cee5f6ce3e3916651a522e8a84f2eb18f6
razineAl <razineal@gmail.com>
2025-02-16 22:13:30 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+// declaring node modules we need : 
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+// declaring our own modules :
+
+const dbConn = require('./config/dbConn');
+
+
+const PORT = process.env.PORT;
+
+
+// calling the DB connection module (connect the database)
+dbConn();
+
+
+// some middlewares to properly handle request formats and cors 
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(cors());
+
+
+
+
+
+
+
+
+
+
+
+// starting the server and the DB
+
+mongoose.connection.once('open',()=>{
+    console.log('connected to mongodb');
+    app.listen(PORT,()=>{
+        console.log('server running on port :',PORT);
+    })
+});

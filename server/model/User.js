1f4b0cd709c046c4502682fe9a31197692f026a1
razineAl <razineal@gmail.com>
2025-02-28 17:23:43 +0100

    role middleware

--- server/model/User.js
+++ server/model/User.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         required: true,
-        default: 'visitor'
+        default: process.env.VISITOR_ROLE
     },
     userValide: {
         type: Boolean,

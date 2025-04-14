3a9c6d4de713d4fadf4c146363cbd9a709513202
Rahim Sarah <ns_rahim@esi.dz>
2025-04-14 20:07:45 +0100

    sidebar + navbar

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,44 @@
-import logo from '../assets/footer-white.png'
-import '../componentsStyles/Footer.css'
+import { useState } from "react";
+import {FaSearch} from "react-icons/fa";
+import "../componentsStyles/SearchBar.css";
+import ijj from "../assets/gg_profile.svg" 
+import { useNavigate } from "react-router-dom";
 
-export default function Footer() {
-    return (
-        <div className="footer">
-            <img className="footer-logo" src={logo} alt="Logo" />
-            <p className="footer-text">
-                EQUIPE14 | <span className="footer-brand">ATHAR</span> © 2025
-            </p>
-        </div>
-    )
+function SearchBar({ onSearch }) {
+
+  const navigate = useNavigate(); 
+  const goToProfil = () => {
+    navigate("/profil")
+  }
+
+  const [query, setQuery] = useState("");
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value); // Pass search query to parent component
+  };
+
+  return  (
+    <div className="search-div sticky top-0 px-5 py-6 bg-white  z-[9999] relative flex items-center">
+      <div className="relative w-full  max-w-[88%] mx-auto">
+        <input 
+          type="text"
+          placeholder="Rechercher Un Projet..."
+          value={query}
+          onChange={handleChange}
+          className="search-input "
+        />
+        <FaSearch className="search-icon" />
+      </div>
+      <img
+        src={ijj}
+        alt="Profil"
+        className="prf absolute right-5"
+        onClick={goToProfil}
+      />
+    </div>
+  );
+  
 }
+
+export default SearchBar;
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import {FaSearch} from "react-icons/fa";
 import "../componentsStyles/SearchBar.css";
+import ijj from "../assets/gg_profile.svg" 
+import { useNavigate } from "react-router-dom";
 
 function SearchBar({ onSearch }) {
+
+  const navigate = useNavigate(); 
+  const goToProfil = () => {
+    navigate("/profil")
+  }
+
   const [query, setQuery] = useState("");
 
   const handleChange = (e) => {
@@ -10,19 +18,27 @@ function SearchBar({ onSearch }) {
     onSearch(e.target.value); // Pass search query to parent component
   };
 
-  return (
-    <div className="search-container">
-       <input
-      type="text"
-      placeholder="Search..."
-      value={query}
-      onChange={handleChange}
-      className="search-input"
-    />
-    <FaSearch className="search-icon" /> 
+  return  (
+    <div className="search-div sticky top-0 px-5 py-6 bg-white  z-[9999] relative flex items-center">
+      <div className="relative w-full  max-w-[88%] mx-auto">
+        <input 
+          type="text"
+          placeholder="Rechercher Un Projet..."
+          value={query}
+          onChange={handleChange}
+          className="search-input "
+        />
+        <FaSearch className="search-icon" />
+      </div>
+      <img
+        src={ijj}
+        alt="Profil"
+        className="prf absolute right-5"
+        onClick={goToProfil}
+      />
     </div>
-   
   );
+  
 }
 
 export default SearchBar;
